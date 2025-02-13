665eeec3318ae62e01cd0ed4e7c9abd72b86ff78
mehedi-2024 <sheikhmh33@gmail.com>
2025-02-13 12:35:18 +0600

    add vercel.json

--- src/components/Dashboard/Cart.jsx
+++ src/components/Dashboard/Cart.jsx
@@ -6,26 +6,35 @@ import CartItem from "./CartItem";
 
 const Cart = () => {
     const cartItems = useContext(CartContext)
+    const data = useContext(Data)
     console.log(cartItems[0])
     const totalMoney = useContext(TotalMoney)
 
+    function sort(params) {
+        cartItems[0].sort((a, b) => {
+            return data[b-1].price - data[a-1].price
+        })
+        console.log(cartItems[0])
+    }
     return (
         <div className='mt-10'>
             <div className='flex justify-between'>
                 <h2 className="text-3xl font-bold">Cart</h2>
                 <div className='flex gap-3'>
                     <h2 className="text-2xl font-bold">Total cost : ${totalMoney[0]}</h2>
-                    <button className='font-bold px-4 py-1 rounded-3xl bg-pink-100 border-2 text-sm border-purple-600 flex gap-2 items-center'>Sort by Price<PiSortAscendingBold size={20} /></button>
+                    <button onClick={sort} className='font-bold px-4 py-1 rounded-3xl bg-pink-100 border-2 text-sm border-purple-600 flex gap-2 items-center'>Sort by Price<PiSortAscendingBold size={20} /></button>
                     <button className='font-bold px-4 py-1 rounded-3xl bg-purple-600 text-white text-sm border-2 border-purple-600 '>Purchase </button>
                 </div>
             </div>
 
-            <div className="mt-8 flex flex-col gap-6">
+            {
+                cartItems[0].length ? <div className="mt-8 flex flex-col gap-6">
                 {
-                    cartItems[0].map(id =>  <CartItem id={id} /> )
+                    cartItems[0].map(id => <CartItem id={id} />)
                 }
-            </div>
-        </div>
+            </div> : <h1 className="text-4xl font-bold text-purple-600 text-center mt-8">No data to show statistics</h1>
+            }
+        </div >
     )
 }
 
--- src/components/Dashboard/Wishlist.jsx
+++ src/components/Dashboard/Wishlist.jsx
@@ -9,8 +9,9 @@ const Wishlist = () => {
     <div className='mt-10'>
       <h2 className="text-3xl font-bold">Wishlist :</h2>
       <div className="mt-8 flex flex-col gap-6">
+
         {
-          wishlistItems[0].map(item => <WishlistItem id={item}></WishlistItem>)
+          wishlistItems[0].length ?  wishlistItems[0].map(item => <WishlistItem id={item}></WishlistItem>) : <h1 className="text-4xl font-bold text-purple-600 text-center mt-8">No data to show statistics</h1>
         }
       </div>
     </div>
